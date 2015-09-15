42d979032d47b01bd996ce8b52e119b488e0547b
Troy-S <tsheed@hotmail.co.uk>
2015-09-15 15:57:50 +0100

    User model created - Setting up passport.js in config file

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 var express  = require("express");
 var app      = express();
 var mongoose = require("mongoose");
+var passport = require(z)
 
 mongoose.connect("mongodb://localhost:27017/tweetsteam")
 
 app.set("views", "./views");
 app.set('view engine', "ejs");
+
+// Setup Passport
+require('./config/passport')(passport);
 
 // Serve all js, css, html from the public folder
 app.use(express.static(__dirname + '/public'));
